b87db2a8838685beeba9e026e0a8d7544aec9a01
tarunnjoshi <tarun12joshi@gmail.com>
2023-12-02 13:31:24 +0530

    Adding prop passing prop

--- 03tailwind props/src/App.jsx
+++ 03tailwind props/src/App.jsx
@@ -2,34 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Cart from './components/Cart.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
+	let myObj = {
+		name: 'Tarun',
+		secondname: 'TarunJoshi',
+		age: 23,
+		course: 'react'
+	}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+		<h1 className='bg-green-400 p-5 rounded-xl mb-5'>Tailwind css</h1>
+		<Cart customObj={myObj.name} btnText="Click Tarun"/>
+		<Cart customObj="Tarun Joshi" btnText="Tarun Joshi"/>
+	</> 
+	)
+
 }
-
 export default App
